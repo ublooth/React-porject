d6179ee4840c90bab60ed490bfbff468bf01e4c9
ublooth <695927047@qq.com>
2019-09-09 15:19:36 +0800

    react 0.1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+// 入口文件
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; // 导入组件
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')); // <App />:组件，第4行引入的，挂载在#root
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
