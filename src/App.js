2c3e6664c3f2e40ee14d1a18b9cccaba0fc656bb
ublooth <695927047@qq.com>
2019-09-10 13:30:08 +0800

    react-1知识点1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Header from './components/Header'; // 组件Header
+import Home from './components/Home'; // 组件Home
 
 function App() {
+  const user = {
+    name: 'Anna',
+    hobbies: ['Sports','Reading']
+  }
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div className="container">
-      <div className="roe">
+      <div className="row">
+        <div className="col-xs-1 col-xs-offset-11">
+          <Header />
+        </div>
+      </div>
+      <div className="row">
         <div className="col-xs-1 col-xs-offset-11">
           <h1>Hell World !!</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-1 col-xs-offset-11">
+          <Home name={'Max'} initialAge={12} user={ user }>
+            <p>父元素Home的子节点,使用this.props.children输出</p>
+          </Home>
+          {/*  name={'Max'} initialAge={12} user={user} 传递数据 */}
         </div>
       </div>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-xs-1 col-xs-offset-11">
+          <h1>Header !!!!</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
