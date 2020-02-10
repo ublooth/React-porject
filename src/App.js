3b1adcdfaa17d04b07c32f37dd03b39927c2c19d
ublooth <695927047@qq.com>
2020-02-10 21:00:30 +0800

    react  ele

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+  
+//   return (
+//     <div className="App">
+//       123123
+//     </div>
+//   );
+// }
+
+import React, { Component } from 'react';
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  componentDidMount() {
+    this.getSliderList();
+  }
+  getSliderList(){
+    // axios.get(`/v1/cities?type=hot`)
+    //   .then((res) => {
+    //     console.log('请求返回的数据');
+    //     console.log(res.data)//此接口返回数据为jsonp格式，须进一步对数据进行处理
+    //   });
+  }
+  render() {
+    return (
+      <div className="App">
+        123123
+      </div>
+    );
+  }
 }
 
 export default App;
