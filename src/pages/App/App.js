0219d0ff106fc41a3718cae8ae02d3e07c9637a3
ublooth <695927047@qq.com>
2020-02-14 14:56:25 +0800

    '对象数据循环遍历处理方式'

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -7,30 +7,50 @@ import { getPopCityList, getAllCityList } from '../../api/api.js';
 
 class App extends Component {
   constructor(props) {
-    super(props); 
-    this.state = { 
+    super(props);
+    this.state = {
       age: 11,
       url: '1212',
       cityOne: [], // 热门城市
+      allCity: {}, // 全部城市
     }
   }
   componentDidMount() {
     this.getSliderList();
+    this.set();
   }
-  async getSliderList() {
-    await getPopCityList({type: 'hot'}).then(res => {
-      this.setState({
-        cityOne: res.data
-      })
+  getSliderList() {
+    getPopCityList({ type: 'hot' }).then(res => {
+      if (res.status === 200) {
+        this.setState({
+          cityOne: res.data
+        })
+      }
     })
-    await getAllCityList({type: 'group'}).then(res => {
-      console.log("res", res)
+    getAllCityList({ type: 'group' }).then(res => {
+      if (res.status === 200) {
+        this.setState({
+          allCity: res.data
+        });
+        this.setAllCity();
+      }
     })
   }
   async set() {
     await this.setState({
-        url: this.props.match.path
+      url: this.props.match.path
     })
+  }
+  setAllCity() {
+    let arr = {};
+    for (let i = 65; i <= 90; i++) {
+      if (this.state.allCity[String.fromCharCode(i)]) {
+        arr[String.fromCharCode(i)] = this.state.allCity[String.fromCharCode(i)]
+      }
+    }
+    this.setState({
+      allCity: arr
+    });
   }
   getId(id) {
     console.log("id", id)
@@ -41,7 +61,7 @@ class App extends Component {
         <Head></Head>
         <div className="App-current">
           <div className="app-currentHead">
-            <span>当前定位城市：</span>
+            <span>当前定位城市：{this.state.url}</span>
             <span>定位不准时，请在城市列表中选择</span>
           </div>
           <div className="app-current-choice">
@@ -51,19 +71,41 @@ class App extends Component {
         <div className="app-city-str">
           <div className="strHear">热门城市</div>
           <ul className="strLIist">
-            { this.state.cityOne.map(
+            {this.state.cityOne.map(
               (item, i) => <li
-                onClick={ () => { this.getId(item.id) } }
-                key={ i }
-              >{ item.name }</li>
-            ) }
+                onClick={() => { this.getId(item.id) }}
+                key={i}
+                className="bule"
+              >{item.name}</li>
+            )}
           </ul>
         </div>
+        {
+          Object.keys(this.state.allCity).map(
+            (item, i) => <div className="app-city-all"
+              key={i}
+            >
+              <div className="allHead">
+                <span>{item}</span>
+                <span style={i === 0 ? { display: 'inline-block' } : { display: 'none' }}>（按字母排序）</span>
+              </div>
+              <ul className="strLIist">
+                {this.state.allCity[item].map((str, z) =>
+                  <li
+                    key={z}
+                    onClick={() => { this.getId(str.id) }}
+                  >
+                    {str.name}
+                  </li>
+                )}
+              </ul>
+            </div>
+          )
+        }
         <Link to='/city'>123123</Link>
       </div>
     );
   }
-
 }
 
 export default App;
